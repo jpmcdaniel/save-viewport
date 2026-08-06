41c9998712e5274c11e31559cab0b601d2bebaf3
John McDaniel <johnmcdaniel@gmail.com>
2026-08-06 12:59:51 -0400

    change from a hello to a functional script

--- main.js
+++ main.js
@@ -1,5 +1,10 @@
 import OBR from "@owlbear-rodeo/sdk";
 
 OBR.onReady(() => {
-  document.getElementById('status').innerText = "Connected to room successfully!";
+  // Listen for scene ready state changes
+  OBR.scene.onReadyChange((isReady) => {
+    if (isReady) {
+      OBR.notification.show("Scene loaded!");
+    }
+  });
 });
